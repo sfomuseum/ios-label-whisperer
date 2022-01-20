b89519b6ea19bdd001126bd7ad8589a4dcfcf7cb
thisisaaronland <aaron@localhost>
2022-01-19 17:18:40 -0800

    snapshot: compare files, write new data; still needs callback methods

--- LableWhisperer/DefinitionFiles.swift
+++ LableWhisperer/DefinitionFiles.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import AccessionNumbers
+import CryptoKit
 
 public enum DefinitionFilesErrors: Error {
     case invalidRoot
@@ -43,7 +44,7 @@ public class DefinitionFiles {
         case .success(let results):
             urls = results
         }
-                
+        
         for u in urls {
             
             let def_rsp = self.LoadFromURL(url: u)
@@ -69,7 +70,7 @@ public class DefinitionFiles {
         var req = URLRequest(url: url)
         req.httpMethod = "GET"
         req.timeoutInterval = TimeInterval(10)
-                
+        
         let task = URLSession.shared.dataTask(with: req) { [self] data, response, error in
             
             guard let rsp = response as? HTTPURLResponse else {
@@ -97,7 +98,7 @@ public class DefinitionFiles {
             for f in filenames {
                 self.RefreshDataFile(filename: String(f))
             }
-    
+            
         }
         
         task.resume()
@@ -113,7 +114,7 @@ public class DefinitionFiles {
         var req = URLRequest(url: url)
         req.httpMethod = "GET"
         req.timeoutInterval = TimeInterval(10)
-                
+        
         let task = URLSession.shared.dataTask(with: req) { [self] data, response, error in
             
             guard let rsp = response as? HTTPURLResponse else {
@@ -135,14 +136,53 @@ public class DefinitionFiles {
                 return
             }
             
+            let remote_hashed = SHA256.hash(data: data!)
+            var write_data = false
+            
+            let local_path = Bundle.main.resourcePath! + "/data.bundle/" + filename
+            let local_uri = "file://" + local_path
+            let local_url = URL(string: local_uri)
+            
+            if local_url != nil {
+                
+                let data_rsp = ReadFromURL(url: local_url!)
+                
+                switch data_rsp {
+                case .failure(let error):
+                    print("Failed to read \(local_url) because \(error)")
+                    write_data = true
+                case .success(let local_data):
+                    
+                    let local_hashed = SHA256.hash(data: local_data)
+                    
+                    if local_hashed != remote_hashed {
+                        write_data = true
+                    }
+                }
+                
+            } else {
+                write_data = true
+            }
+            
+            if !write_data {
+                return
+            }
+            
             print("WRITE DATA \(filename)")
+            
+            do {
+                try data!.write(to: local_url!)
+            } catch (let error) {
+                print("SAD WRITE \(error)")
+            }
+            
         }
         
         task.resume()
         return .success(true)
     }
     
-    public func List() -> Result<[URL], Error> {                
+    public func List() -> Result<[URL], Error> {
         
         var directoryContents: [URL]
         
@@ -159,7 +199,7 @@ public class DefinitionFiles {
     public func Load() -> Result<[Definition], Error> {
         
         var definitions = [Definition]()
-
+        
         // Remember url_map is built during the initializer
         
         for (_, u) in self.url_map {
@@ -178,7 +218,7 @@ public class DefinitionFiles {
     }
     
     public func LoadFromOrganizationURL(organization_url: String) -> Result<Definition, Error> {
-
+        
         guard let definition_url = self.url_map[organization_url] else {
             return .failure(DefinitionFilesErrors.notFound)
         }
@@ -191,10 +231,13 @@ public class DefinitionFiles {
         var data: Data
         var def: Definition
         
-        do {
-            data = try Data(contentsOf: url)
-        } catch (let error){
+        let data_rsp = ReadFromURL(url: url)
+        
+        switch data_rsp {
+        case .failure(let error):
             return .failure(error)
+        case .success(let d):
+            data = d
         }
         
         let decoder = JSONDecoder()
@@ -207,4 +250,18 @@ public class DefinitionFiles {
         
         return .success(def)
     }
+    
+    private func ReadFromURL(url: URL) -> Result<Data, Error> {
+        
+        var data: Data
+        
+        do {
+            data = try Data(contentsOf: url)
+        } catch (let error){
+            return .failure(error)
+        }
+        
+        return .success(data)
+    }
+    
 }
